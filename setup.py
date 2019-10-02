89fe9842d55580191034afd99672f403b3775ba8
Zach <zachnguyen0111@gmail.com>
2019-10-01 23:15:50 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+
+from distutils.core import setup
+
+
+def readme():
+    """Import the README.md Markdown file and try to convert it to RST format."""
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError):
+        with open('README.md') as readme_file:
+            return readme_file.read()
+
+
+setup(
+    name='titanic',
+    version='0.1',
+    description='Analysis of the Titanic dataset',
+    long_description=readme(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    url='https://github.com/zachattack_0111/titanic_datascience',
+    author='Zach nguyen',
+    author_email='nguyenngochaiizz@gmail.com', 
+    license='MIT',
+    packages=['titanic'],
+)
